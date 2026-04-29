989d1f4e8ffeacf5488f21bb1e243fc9ab114fa6
Yoben 250 <uwayoben11@gmail.com>
2026-04-29 08:57:45 +0200

    fix: switch to default prisma-client-js output, remove prisma.config.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import { generateSchedule, classifyLoan } from "../lib/loan-schedule";
 
